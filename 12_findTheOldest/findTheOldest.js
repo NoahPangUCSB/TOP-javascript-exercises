5a8cf805d829b117d70510ba8eb6d6fe77071a46
Noah Pang <noahpang@cs.ucsb.edu>
2023-07-28 15:59:11 -0700

    finished js exercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const peopleByAge = people.sort(function(personA, personB) {
+        personAAge = "yearOfDeath" in personA ? personA.yearOfDeath - personA.yearOfBirth : 
+                            (new Date).getFullYear() - personA.yearOfBirth;
+        personBAge = "yearOfDeath" in personB ? personB.yearOfDeath - personB.yearOfBirth : 
+                            (new Date).getFullYear() - personB.yearOfBirth;
+        return personAAge < personBAge ? 1 : -1;
+    });
 
+    return peopleByAge[0];
 };
 
 // Do not edit below this line
